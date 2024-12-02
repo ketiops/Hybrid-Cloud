a991e0cb0e24b63f9ab5201efcbe9040e5eae2f6
chromatices <pjh403@naver.com>
2024-12-02 16:42:48 +0900

    [Update] Update NodePort & Namespace Allocation

--- pipeline_crd/argo_install.sh
+++ pipeline_crd/argo_install.sh
@@ -3,13 +3,18 @@
 nodeport=$1
 namespace=$2
 
-#customizing nodeport for argo server
-python3 nodeport_change.py --port=$nodeport
-
 # create ns & install argo workflow
 kubectl create namespace $namespace
-kubectl apply -f argo_cluster_resource.yaml
-kubectl apply -n $namespace -f argo_install.yaml
+
+# namespace & nodeport apply on argo_cluster_resource_tem.yaml & argo_install_tmp.yaml
+export NAMESPACE=$namespace
+export NODEPORT=$nodeport
+envsubst < argo_cluster_resource.yaml > argo_cluster_resource_tmp.yaml
+envsubst < argo_install.yaml > argo_install_tmp.yaml
+
+kubectl apply -f argo_cluster_resource_tmp.yaml
+kubectl apply -n $namespace -f argo_install_tmp.yaml
+rm argo_cluster_resource_tmp.yaml argo_install_tmp.yaml
 
 # pipeline runner crd install
 kubectl apply -f ./pipeline_runner/pipeline-runner-role.yaml -f ./pipeline_runner/pipeline-runner-rolebinding.yaml -f ./pipeline_runner/pipeline-runner-sa.yaml -n $namespace
