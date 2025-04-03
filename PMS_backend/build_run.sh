b1a79e6792917064f28d2c359c6a43af954dda29
chromatices <pjh403@naver.com>
2025-04-03 15:25:18 +0900

    [Update] Add Versioning Option

--- PMS_backend/build_run.sh
+++ PMS_backend/build_run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-docker build . -t api_wrapper:0.1
-docker run --rm -itd --name wrapper_test -p 32000:32000 api_wrapper:0.1
+version=$1
+
+docker build . -t chromatices/api_wrapper_strato:${version}
+docker run --rm -itd --name wrapper_test -p 32000:32000 chromatices/api_wrapper_strato:${version}
